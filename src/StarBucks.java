0bc6930864fd86411a899f977a53e57fe336bec4
Touya <bidakhmetovaasem07@gmail.com>
2025-11-02 15:24:21 +0500

    new change commit

---
+++ src/StarBucks.java
@@ -0,0 +1,13 @@
+public class StarBucks extends Establishment {
+
+    public StarBucks() {
+        super("Starbucks");
+        menu.add(new MenuItem("Latte", 1800));
+        menu.add(new MenuItem("Americano", 1700));
+        menu.add(new MenuItem("Tea", 800));
+        menu.add(new MenuItem("Sandwich", 1000));
+    }
+}
+
+
+
---
+++ src/Tami.java
@@ -0,0 +1,11 @@
+public class Tami extends Establishment {
+
+    public Tami() {
+        super("Tami");
+        menu.add(new MenuItem("Lagman", 3000));
+        menu.add(new MenuItem("Pizza", 2000));
+        menu.add(new MenuItem("Tea", 700));
+        menu.add(new MenuItem("Manty", 1600));
+    }
+}
+
